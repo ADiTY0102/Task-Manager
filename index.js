bfc584f637cc2ffcbf28a6ff70caefbcda48cf3c
ADiTY0102 <adityabinjagermath12@gmail.com>
2026-02-14 15:26:54 +0530

    Enhanced edit functionality for edit fixxes from the task manager ---#3

--- index.js
+++ index.js
@@ -29,23 +29,30 @@ app.get("/edit/:filename", function (req, res) {
     res.render("edit", { filename: req.params.filename });
 })
 app.post("/edit", function (req, res) {
-    fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`, (err) => {
-        if (err) {
-            console.log("There is an error in renaming file",err);
+    const oldPath = `./files/${req.body.previous}`;
+    const newPath = `./files/${req.body.new}`;
+
+    // Check if the "delete" button was pressed
+    if (req.body.action === 'delete') {
+        fs.unlink(oldPath, (err) => {
+            if (err) console.log("Error deleting file", err);
+            res.redirect("/");
+        });
+    } 
+    // Otherwise, it's a rename (Confirm)
+    else {
+        // Only rename if a new name was actually provided
+        if (req.body.new && req.body.new !== req.body.previous) {
+            fs.rename(oldPath, newPath, (err) => {
+                if (err) console.log("Error renaming file", err);
+                res.redirect("/");
+            });
         } else {
-            console.log("File Renamed Successfully");
+            // If no new name, just go back home
             res.redirect("/");
         }
-    })
-    fs.unlink(`./files/${req.body.previous}`,(err) => {
-        if (err) {
-            console.log("There is an error in deleting file",err);
-        } else {
-            console.log("File Deleted Successfully");
-            res.redirect("/");
-        }
-    })
-})
+    }
+});
 
 
 app.post("/create", function (req, res) {
